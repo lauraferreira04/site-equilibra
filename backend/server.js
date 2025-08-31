8db6382b2edae3ec2e5edc4e60222c561df75bf5
lauraferreira04 <lauraferreiraa015@gmail.com>
2025-08-31 00:26:53 -0300

    backend [REFACTOR]: preparar para deploy no Render.

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");   // <- import
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
